406f4c2ab5b29b0c138ac124d21b79b04f97ab24
zurlocke <fnpocontact@gmail.com>
2016-04-11 21:06:41 -0400

    Update bernfinder.js

--- bernfinder.js
+++ bernfinder.js
@@ -5,6 +5,6 @@
 // @description  ttvtesting
 // @author       zurlocke
 // @include      https://twitter.com/*
-// @updateURL    
+// @updateURL    https://raw.githubusercontent.com/zurlocke/bernfinder/master/bernfinder.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
