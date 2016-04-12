ec9e5e396063a0b2a0819415a2d49b1ae1469f8b
zurlocke <fnpocontact@gmail.com>
2016-04-11 21:06:07 -0400

    Create bernfinder.js

---
+++ bernfinder.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         ttvtest
+// @namespace    https://tampermonkey.net/
+// @version      0.1
+// @description  ttvtesting
+// @author       zurlocke
+// @include      https://twitter.com/*
+// @updateURL    
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
